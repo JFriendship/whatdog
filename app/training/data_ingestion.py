37ceb33447299f7c9aab32e94691721d2286f595
JFriendship <155485648+JFriendship@users.noreply.github.com>
2026-07-22 12:56:13 -0700

    feat: add lightning data module

--- app/training/data_ingestion.py
+++ app/training/data_ingestion.py
@@ -3,7 +3,12 @@ import xml.etree.ElementTree as ET
 from torch.utils.data import Dataset
 import os
 
-class CroppedStanfordDogsDataset:
+import lightning as L
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader, Subset
+
+class CroppedStanfordDogsDataset(Dataset):
     def __init__(self, images_dir: str, annotations_dir: str, transform=None):
         """
         Args:
@@ -82,3 +87,89 @@ class CroppedStanfordDogsDataset:
             image = self.transform(image)
 
         return image, sample['label'] 
+    
+
+class WhatdogDataModule(L.LightningDataModule):
+    def __init__(self, images_dir: str = "./data/Images", annotations_dir: str = "./data/Annotation", batch_size: int = 32, num_workers: int = 2):
+        super().__init__()
+        self.images_dir = images_dir
+        self.annotations_dir = annotations_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        # Normalize based on Imagenet1k dataset
+        self.normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], 
+            std=[0.229, 0.224, 0.225]
+        )
+
+        self.train_transform = transforms.Compose([
+            transforms.Resize(size=(256, 256)),
+            transforms.CenterCrop(224),
+            transforms.RandomHorizontalFlip(0.4),
+            transforms.RandomRotation(0.15),
+            transforms.ColorJitter(brightness=0.2),
+
+            transforms.ToTensor(),
+            self.normalize
+        ])
+
+        # The test_val transform will be used for the test dataset and the validation dataset
+        self.test_val_transform = transforms.Compose([
+            transforms.Resize(size=(256, 256)),
+            transforms.CenterCrop(224),
+
+            transforms.ToTensor(),
+            self.normalize
+        ])
+
+    def setup(self, stage: str = None):
+        # Load dataset
+        full_train_dataset = CroppedStanfordDogsDataset(
+            images_dir=self.images_dir, 
+            annotations_dir=self.annotations_dir, 
+            transform=self.train_transform
+        )
+        full_test_val_dataset = CroppedStanfordDogsDataset(
+            images_dir=self.images_dir, 
+            annotations_dir=self.annotations_dir, 
+            transform=self.test_val_transform
+        )
+        # Split dataset
+        total_dataset_size = len(full_train_dataset)
+        train_size = int(total_dataset_size * 0.7)
+        val_size = int(total_dataset_size * 0.15)
+        # Test size implied 15%
+
+        # Generate fixed split indices for consistent results
+        generator = torch.Generator().manual_seed(24)
+        indices = torch.randperm(total_dataset_size, generator=generator).tolist()
+
+        # Create subsets using generated indices
+        self.train_data = Subset(full_train_dataset, indices[:train_size])
+        self.val_data = Subset(full_test_val_dataset, indices[train_size:train_size+val_size])
+        self.test_data = Subset(full_test_val_dataset, indices[train_size+val_size:])
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.train_data, 
+            batch_size=self.batch_size, 
+            shuffle=True, 
+            num_workers=self.num_workers
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.val_data,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.test_data,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
+        )
