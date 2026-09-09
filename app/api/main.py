5cad7893594575c0f055ad1248221d455c87eaae
JFriendship <155485648+JFriendship@users.noreply.github.com>
2026-09-09 16:22:04 -0700

    feat: add model inference to fastapi lifespan
    Loads the specified model checkpoint into the fastapi lifespan
    - The model only needs to be loaded once

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,32 @@
+import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from pathlib import Path
 
-app = FastAPI(title="Whatdog API", version="2.0.0")
+from app.api.inference import PyTorchInference
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    checkpoint_path = Path(os.environ["CHECKPOINT_PATH"])
+    class_names_path = Path(os.environ["CLASS_NAMES_PATH"])
+
+    if not checkpoint_path.is_file():
+        raise FileNotFoundError(f"checkpoint file not found: {checkpoint_path}")
+    
+    if not class_names_path.is_file():
+        raise FileNotFoundError(f"class names file not found: {class_names_path}")
+    
+    print("LOADING WHATDOG MODEL")
+    app.state.inference = PyTorchInference(checkpoint_path=checkpoint_path, class_names_path=class_names_path)
+    print("FINISHED LOADING WHATDOG MODEL")
+
+    yield
+
+    print("TEARING DOWN WHATDOG MODEL")
+    del app.state.inference
+    print("WHATDOG MODEL SUCCESSFULLY TORN DOWN")
+
+app = FastAPI(title="Whatdog API", version="2.0.0", lifespan=lifespan)
 
 @app.get("/health")
 def health():
